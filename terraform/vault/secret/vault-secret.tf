30ae3efc63cdcdfe088c381041c7c11a5b6a5fc2
Sebastián Gómez <sebastiangomezcorrea@gmail.com>
2023-04-16 19:37:03 -0400

    More secrets and ExternalSecrets
    Signed-off-by: Sebastián Gómez <sebastiangomezcorrea@gmail.com>

--- terraform/vault/secret/vault-secret.tf
+++ terraform/vault/secret/vault-secret.tf
@@ -1,6 +1,6 @@
-resource "vault_kv_secret_v2" "vault-secret" {
+resource "vault_kv_secret_v2" "secret-one" {
   mount               = "secret"
-  name                = "my-secret"
+  name                = "secret-one"
   cas                 = 1
   delete_all_versions = true
   data_json = jsonencode(
@@ -14,6 +14,46 @@ resource "vault_kv_secret_v2" "vault-secret" {
       dev      = "seb",
       provider = "Hashicrop Vault"
       example  = "External Secrets Operator"
+    }
+  }
+}
+
+resource "vault_kv_secret_v2" "secret-two" {
+  mount               = "secret"
+  name                = "secret-two"
+  cas                 = 1
+  delete_all_versions = true
+  data_json = jsonencode(
+    {
+      different-key = "A new secret"
+    }
+  )
+  custom_metadata {
+    max_versions = 5
+    data = {
+      dev      = "sebastian",
+      provider = "Hashicrop Vault"
+      example  = "ESO"
+    }
+  }
+}
+
+resource "vault_kv_secret_v2" "three" {
+  mount               = "secret"
+  name                = "three"
+  cas                 = 1
+  delete_all_versions = true
+  data_json = jsonencode(
+    {
+      key = "Third secret value"
+    }
+  )
+  custom_metadata {
+    max_versions = 5
+    data = {
+      dev      = "seb",
+      provider = "Hashicrop Vault"
+      example  = "ESO"
     }
   }
 }
